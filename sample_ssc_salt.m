d1b36803d8129fe9b1589b53efe58685db359fef
MJFcoNaN <mjfconan@outlook.com>
2019-04-18 20:19:09 +0800

    rename ttt... convert file name to English

--- sample_ssc_salt.m
+++ sample_ssc_salt.m
@@ -15,11 +15,11 @@
 % sector.
 % 
 % =========================================================================
-% kongbai.xlsx: ("ttt2" is not considered now)
+% blank_membrane.xlsx: ("unit" is not considered now)
 %
 %  only one sheet, named "all"
 %
-%               filter_id     ttt         ttt1      blank   ttt2
+%            membrane_id     dt_date    dt_time     blank   unit
 %               7600        2017/1/16   14:22:29    0.7618   g
 %               7601        2017/1/16   14:23:32    0.7839   g
 %               7602        2017/1/16   14:23:50    0.7755   g
@@ -27,18 +27,18 @@
 %               7604        2017/1/16   14:24:20    0.7757   g
 %               ...
 % =========================================================================
-% zongzhong.xlsx: the same as kongbai.xlsx
+% added_membrane.xlsx: the same as blank_membrane.xlsx
 %
 % only one sheet, named "all"
 % 
-%               filter_id     ttt         ttt1        add    ttt2
+%            membrane_id     dt_date    dt_time      add    unit
 %               6200        2017/1/18   9:35:55     0.7849    g
 %               6201        2017/1/18   9:36:28     0.796     g
 %               6205        2017/1/18   9:38:13     0.862     g
 %               6206        2017/1/18   9:38:22     0.803     g
 % 
 % =========================================================================
-% 盐度、悬浮物测定记录表2.xlsx
+% sampleID_salt_membID.xlsx
 % 
 % ##### #####  CAUSTIONS 1 #####  #####  
 % "sample_id" is the number sticked on the glass bottles, 
@@ -48,25 +48,27 @@
 % "color_list", "color_value"
 %
 % ##### #####  CAUSTIONS 2 #####  ##### 
-% If there is more than one filter for any "sample_id", 
-% you must delete others and only left one. ("merge to one filter")
-% The same step should be done in "kongbai.xlsx", "kongbai.xlsx"
-% for example, in "kongbai.xlsx", "1111" is the sum weight for 3 filters,
-% because these 3 filters corresponde to one water sample.
+% If there is more than one membrane for any "sample_id", 
+% you must delete others and only left one. ("merge to one membrane")
+% The same step should be done in "blank_membrane.xlsx" 
+% and "added_membrane.xlsx".
+% For example, in "blank_membrane.xlsx", 
+% "1111" is the sum weight for 3 membranes,
+% because these 3 membranes corresponde to one water sample.
 % 
-%              filter_id      ttt         ttt1        add     ttt2
+%           membrane_id    dt_date      dt_time      add     unit
 %              1111        2017/1/16    20:20:20     2.22      g
 %
-% similar in "zongzhong.xlsx",
+% similar in "added_membrane.xlsx",
 % 
-%              filter_id      ttt         ttt1        add     ttt2
+%           membrane_id    dt_date      dt_time      add     unit
 %              1111        2017/1/26    10:10:10    11.11      g
 %
 % ##### #####          #####  #####  
 %
 %   sheet "light_blue"
 %
-%               sample_id   salt        filter_id
+%               sample_id   salt        membrane_id
 %               281         0.29         7592
 %               396         0.62         6087
 %               1           3.13         7948
@@ -74,12 +76,12 @@
 %
 %   sheet "green"
 %
-%               sample_id   salt     水样容积    filter_id    滤纸号2   滤纸号3
+%               sample_id   salt         membrane_id   
 %               1           0.1884              8872
 %               2           0.1884              8873
 %               3           0.1867              8874
 % =========================================================================
-% "汇总.xlsx"
+% "station_sampleID.xlsx"
 %  Only one sheet, namely "all", will be considered.
 %  Missing data is OK, 
 %  then the sample_id is the base value such as 10000, 20000.
@@ -91,17 +93,17 @@
 %  "color_list", "color_value"
 % ##### #####       #####  #####
 %
-%           site       tide     date        hour   h    layer 瓶号 sample_id
-%           南槽上       小     2016/12/6       8   10     0    2   20002 
-%           南槽上       小     2016/12/6       8   10     0.2  3   20003 
-%           南槽上       小     2016/12/6       8   10     0.4  4   20004 
-%           B3          大     2016/12/16     17   5.6    0.6      10000 
-%           B3          大     2016/12/16     17   5.6    0.8      10000 
-%           B3          大     2016/12/16     17   5.6    1    375 10375 
-%           B3          大     2016/12/16     18   4.2    0    376 10376 
-%           北港中       小     2016/12/8       9   8.6    0.6  184 30184 
-%           北港中       小     2016/12/8       9   8.6    0.8  185 30185 
-%           北港中       小     2016/12/8       9   8.6    1    186 30186 
+%      site   tide     date        hour   h    layer sample_id_0  sample_id
+%      A1      n     2016/12/6       8   10     0    2             20002 
+%      A1      n     2016/12/6       8   10     0.2  3             20003 
+%      A1      n     2016/12/6       8   10     0.4  4             20004 
+%      B3      s     2016/12/16     17   5.6    0.6                10000 
+%      B3      s     2016/12/16     17   5.6    0.8                10000 
+%      B3      s     2016/12/16     17   5.6    1    375           10375 
+%      B3      s     2016/12/16     18   4.2    0    376           10376 
+%      E       n     2016/12/8       9   8.6    0.6  184           30184 
+%      E       n     2016/12/8       9   8.6    0.8  185           30185 
+%      E       n     2016/12/8       9   8.6    1    186           30186 
 %
 % =========================================================================
 %% ========================================================================
@@ -113,15 +115,15 @@ base_value    = 1E4;
 color_list    = {'red', 'white', 'dark_blue', 'light_blue'};
 color_value   = [  1,     2,        3,            4]  * base_value;
 
-data_dir      = '/Users/mjfconan/SSC水样';
+data_dir      = '/Users/mjfconan/measure';
 % blank
-xls_ssc_00    = fullfile(data_dir, 'kongbai.xlsx');
-% added filters
-xls_ssc_01    = fullfile(data_dir, 'zongzhong.xlsx');
-xls_salinity  = fullfile(data_dir, '盐度、悬浮物测定记录表2.xlsx');
-xls_position  = fullfile(data_dir, '汇总.xlsx');
+xls_ssc_00    = fullfile(data_dir, 'blank_membrane.xlsx');
+% added membranes
+xls_ssc_01    = fullfile(data_dir, 'added_membrane.xlsx');
+xls_salinity  = fullfile(data_dir, 'sampleID_salt_membID.xlsx');
+xls_position  = fullfile(data_dir, 'station_sampleID.xlsx');
 
-% if weight again and "zongzhong" has an added sheet named "new_add"
+% if weight again and "added_membrane" has an added sheet named "new_add"
 is_re_weitht              = false; 
 % if you screw up in certain days, you may delete them by time...
 is_delete_some_days_value = false;
@@ -146,17 +148,17 @@ tbl_ssc_02(isnan(tbl_ssc_02.new_add), :) = [];
 
 %%
 % valid data
-tbl_ssc_new = innerjoin(tbl_ssc_00, tbl_ssc_02, 'Keys', 'filter_id');
+tbl_ssc_new = innerjoin(tbl_ssc_00, tbl_ssc_02, 'Keys', 'membrane_id');
 tbl_ssc_new.ssc = (tbl_ssc_new.new_add - tbl_ssc_new.blank)/sample_volume;
 tbl_ssc_new.Properties.VariableNames{'new_add'} = 'add';
 % tbl_ssc_new.datetime(:) = datetime(2017,6,10);
 tbl_ssc_new = ...
-    tbl_ssc_new(:, {'filter_id', 'blank', 'add', 'datetime', 'ssc'});
+    tbl_ssc_new(:, {'membrane_id', 'blank', 'add', 'datetime', 'ssc'});
 
-tbl_ssc_11      = innerjoin( tbl_ssc_00, tbl_ssc_01, 'Keys', 'filter_id');
+tbl_ssc_11      = innerjoin( tbl_ssc_00, tbl_ssc_01, 'Keys', 'membrane_id');
 tbl_ssc_11.diff = tbl_ssc_11.add - tbl_ssc_11.blank;
 tbl_ssc_11.ssc  = tbl_ssc_11.diff / sample_volume;
-tbl_ssc_11      = tbl_ssc_11(:, {'filter_id', 'blank', 'add',...
+tbl_ssc_11      = tbl_ssc_11(:, {'membrane_id', 'blank', 'add',...
     'datetime_tbl_ssc_01', 'ssc'});
 % index of awful data
 if is_delete_some_days_value
@@ -171,11 +173,11 @@ end
 % rename datetime
 tbl_ssc_11.Properties.VariableNames{'datetime_tbl_ssc_01'} = 'datetime';
 %% only add new data where is blank
-[lia, locb] = ismember(tbl_ssc_new.filter_id, tbl_ssc_11.filter_id);
+[lia, locb] = ismember(tbl_ssc_new.membrane_id, tbl_ssc_11.membrane_id);
 % only add
 tbl_ssc_11 = [tbl_ssc_11; tbl_ssc_new(~lia, :)];
 % sort
-tbl_ssc_11 = sortrows(tbl_ssc_11, 'filter_id');
+tbl_ssc_11 = sortrows(tbl_ssc_11, 'membrane_id');
 %
 tbl_ssc = tbl_ssc_11;
 % positon
@@ -190,16 +192,16 @@ for ii = 1:length(color_list)
     tmp_color = color_list{ii};
     tmp_color_value = color_value(ii);
     tbl_salt = readtable(xls_salinity, 'Sheet', tmp_color);
-    tbl_salt = tbl_salt(:, {'sample_id', 'filter_id', 'salt'});
+    tbl_salt = tbl_salt(:, {'sample_id', 'membrane_id', 'salt'});
     tbl_salt.sample_id = tbl_salt.sample_id + tmp_color_value;
     tbl_sample = [tbl_sample; tbl_salt];
 end
 clear tmp_*
-tbl_sample(isnan(tbl_sample.filter_id), :)=[];
+tbl_sample(isnan(tbl_sample.membrane_id), :)=[];
 
 
 %%
-tbl_1        = innerjoin(      tbl_sample, tbl_ssc, 'Keys', 'filter_id');
+tbl_1        = innerjoin(      tbl_sample, tbl_ssc, 'Keys', 'membrane_id');
 tbl          = innerjoin( tbl_pos   , tbl_1  , 'Keys', 'sample_id');
 tbl.datetime = ...
     datetime(datenum(tbl.date)+tbl.hour/24, 'ConvertFrom','datenum');
@@ -246,17 +248,17 @@ writetable(tbl, xls_out)
 function tbl_out = deal_with_ssc_xls_file(xls_path, xls_sheet_name)
 tbl_out = readtable(xls_path, 'Sheet', xls_sheet_name); % blank
 
-tbl_out.datetime = tbl_out.ttt + tbl_out.ttt1;
+tbl_out.datetime = tbl_out.dt_date + tbl_out.dt_time;
 
-tbl_out.ttt = []; tbl_out.ttt1 = []; tbl_out.ttt2 = [];
+tbl_out.dt_date = []; tbl_out.dt_time = []; tbl_out.unit = [];
 
 tbl_out.datetime.Format = 'yyyy-MM-dd HH:mm:ss';
 % unify
 tbl_out = unique(tbl_out, 'rows');
 
 % sort
-tbl_out = sortrows(tbl_out, 'filter_id');
+tbl_out = sortrows(tbl_out, 'membrane_id');
 
-% delete filterid == nan
-tbl_out(isnan(tbl_out.filter_id),:)=[];
+% delete membraneid == nan
+tbl_out(isnan(tbl_out.membrane_id),:)=[];
 end
